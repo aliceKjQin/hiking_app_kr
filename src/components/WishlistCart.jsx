9e83059dd666b07415b47fa24230567c14115df7
alice <aliceqininus@gmail.com>
2024-05-23 21:39:38 +0800

    add save button in wishlist

--- src/components/WishlistCart.jsx
+++ src/components/WishlistCart.jsx
@@ -5,13 +5,17 @@ const WishlistCart = () => {
   const { wishlist, removeFromWishlist } = useContext(WishlistContext);
 
   return (
-    <div className="container-lg lg:container m-auto">
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 bg-red-50">
       {wishlist.map((trail) => (
         <div className="flex flex-col lg:flex-row justify-between mb-1" key={trail.id}>
           <h3>{trail.name} <span className="ml-6"></span>{trail.difficulty}</h3>
           <button onClick={() => removeFromWishlist(trail.id)}>Remove</button>
         </div>
       ))}
+      {/* Hide the Save button if wishlist is empty */}
+      <div className="flex justify-end">
+        {wishlist.length === 0 ? null : <button className="h-[36px] bg-red-400 hover:bg-red-500 text-white px-4 py-2 rounded-lg mb-3">Save</button>}
+      </div>
     </div>
   );
 };
